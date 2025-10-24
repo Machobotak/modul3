3ba6a82d69947e5fe0f484954678f84af622823f
Ayub <mibrahimalayubi@gmail.com>
2025-10-24 16:58:59 +0700

    praktikum update readme

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-            System.out.println("Halo");
-            System.out.println("aneh aneh");
+        int[] data = {64, 25, 22, 12, 11};
+        for (int i = 0; i < data.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < data.length; j++) {
+                if (data[j] < data[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = data[minIndex];
+            data[minIndex] = data[i];
+            data[i] = temp;
+        }
+        System.out.print("Hasil setelah diurutkan: ");
+        for (int num : data) {
+            System.out.print(num + " ");
         }
     }
 }
